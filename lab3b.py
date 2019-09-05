ba692952ccf2fb4b9898a9c2e92c6027adc94861
TanushaS <53893658+TanushaS@users.noreply.github.com>
2019-09-05 12:09:30 +0530

    Add files via upload

---
+++ lab3b.py
@@ -0,0 +1,13 @@
+import random
+import string
+def genpwd():
+  letters=string.ascii_letters+string.digits+"!@#$%^&*"
+  length=random.randint(8,12)
+  return ''.join(random.sample(letters,length))
+flag=1
+while flag:
+   flag=int(input("Enter 1 to get a password or Enter 0 to exit:"))
+   if flag==1:
+     print(genpwd())
+   else:
+      flag=0   
