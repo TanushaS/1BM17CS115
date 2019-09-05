03545d1e0d6cb6210a650bb56f2f5a9dace06c40
TanushaS <53893658+TanushaS@users.noreply.github.com>
2019-09-05 12:55:01 +0530

    Update lab3b.py

--- lab3b.py
+++ lab3b.py
@@ -1,13 +1,23 @@
 import random
 import string
-def genpwd():
-  letters=string.ascii_letters+string.digits+"!@#$%^&*"
-  length=random.randint(10,15)
-  return ''.join(random.sample(letters,length))
-flag=1
-while flag:
-   flag=int(input("Enter 1 to get a password or Enter 0 to exit:"))
-   if flag==1:
-     print(genpwd())
-   else:
-      flag=0   
+def func1():
+    chars1=string.ascii_uppercase
+    chars2=string.ascii_lowercase
+    chars3=string.digits
+    chars4='!@#$%^&*'
+    c=string.ascii_letters+string.digits+'!@#$%^&*'
+    size=random.randint(10,15)
+    p= "".join(random.choice(chars1)+random.choice(chars2)+random.choice(chars3)+random.choice(chars4))
+    size-=4
+    p+=''.join(random.choice(c) for j in range(size-1))
+    return ''.join(random.sample(p,len(p)))
+print("1.New password:")
+print("2.Exit")
+print("Enter your choice:")
+ch=int(input())
+while(ch==1):
+    print("Password:",func1())
+    print("Enter your choice:")
+    ch=int(input())
+    
+
